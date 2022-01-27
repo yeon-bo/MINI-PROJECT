7ac7adb80bef6897f64cc7cf416c4bce43acf9ad
yeon-bo <80714114+yeon-bo@users.noreply.github.com>
2022-01-28 06:05:17 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./styles.css";
 
 import { Search } from "../Component/Search";
+import { Topbar } from "../Component/Topbar";
 
 export default function App() {
   return (
     <div className="App">
+      <Topbar />
       <Search />
     </div>
   );
