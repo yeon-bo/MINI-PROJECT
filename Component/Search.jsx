151249e606ec5470b85b473ec46620e4b287ef12
yeon-bo <80714114+yeon-bo@users.noreply.github.com>
2022-01-28 05:02:54 +0900

    homecss-ing

--- Component/Search.jsx
+++ Component/Search.jsx
@@ -5,10 +5,19 @@ export const Search = () => {
   const Search = styled.div`
     background-color: #18191e;
     color: #fff;
+    position: relative;
+    font-family: "Noto Sans KR", sans-serif;
+    font-family: "Roboto", sans-serif;
+    font-size: 14px;
+  `;
+  const Img = styled.img`
+    position: absolute;
+    width: 1.86em;
+    left: 1.43em;
   `;
   return (
     <Search>
-      <img src="./image/logo.png" alt="logo" />
+      <Img src="./image/logo.png" alt="logo" />
       <p>hello</p>
     </Search>
   );
